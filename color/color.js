1149b568ebbc024de77a033d8fadcef4a4651339
jitiendran <jitiendranjiji2000@gmail.com>
2021-02-05 22:17:24 +0530

    color page added

---
+++ color/color.js
@@ -0,0 +1,34 @@
+const div = document.getElementById('wrapper')
+function copy(id,hex){
+    const copytext = document.getElementById(id).textContent
+    navigator.clipboard.writeText(copytext).then(()=>{
+        const copywrap = document.getElementById("s"+id)
+        copywrap.style.display = 'block'
+        copywrap.style.border = `2.5px solid ${hex}`
+        copywrap.style.borderRadius = ".3em"
+        copywrap.style.padding = ".1em .3em"
+        copywrap.style.fontWeight = "500"
+        setTimeout(() => {
+            document.getElementById("s"+id).style.display = 'none'
+        }, 3000);
+    })
+}
+fetch("./color.json")
+    .then((resp)=>{return resp.json()})
+    .then((data)=>{
+        for(let i = 0 ; i < data[0].color.length ; i++){
+            div.innerHTML += 
+            `<div class="color-container">
+                <div class="color" style="background-color: ${data[0].color[i].hex}"></div>
+                <div class="copy-wrapper" id="copy">
+                    <h3 id="${data[0].color[i].id}">${data[0].color[i].hex}</h3>
+                    <div id="${"s"+data[0].color[i].id}" style="display:none"><p>Copied!</p></div>
+                    <button onclick="copy('${data[0].color[i].id}','${data[0].color[i].hex}')"><i class="far fa-copy"></i></button>
+                </div>
+            </div>`
+        }
+    })
+
+const search = ()=>{
+    // type search code here.....
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+const goColor = ()=>{
+    window.location.href = "./color/color.html"
+}
