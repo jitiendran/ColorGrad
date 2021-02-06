906a9ace47ffea263704708fe90e492e4d76d185
jitiendran <jitiendranjiji2000@gmail.com>
2021-02-06 23:00:28 +0530

    Added search feature

--- color/color.js
+++ color/color.js
@@ -2,18 +2,19 @@ const div = document.getElementById('wrapper')
 const copy = (id,hex)=>{
     const copytext = document.getElementById(id).textContent
     navigator.clipboard.writeText(copytext).then(()=>{
-        const copywrap = document.getElementById("s"+id)
-        copywrap.style.display = 'block'
-        copywrap.style.border = `2.5px solid ${hex}`
-        copywrap.style.borderRadius = ".3em"
-        copywrap.style.padding = ".1em .3em"
-        copywrap.style.fontWeight = "500"
+        const copywrap = document.getElementById("s"+id).style
+        copywrap.display = 'block'
+        copywrap.border = `2.5px solid ${hex}`
+        copywrap.borderRadius = ".3em"
+        copywrap.padding = ".1em .3em"
+        copywrap.fontWeight = "500"
         setTimeout(() => {
             document.getElementById("s"+id).style.display = 'none'
         }, 3000);
     })
 }
-fetch("./color.json")
+const fetched = ()=>{
+    fetch("./color.json")
     .then((resp)=>{return resp.json()})
     .then((data)=>{
         for(let i = 0 ; i < data.color.length ; i++){
@@ -28,7 +29,38 @@ fetch("./color.json")
             </div>`
         }
     })
+}
 
+// Fetching on loading
+fetched()
 const search = ()=>{
-    // type search code here.....
+    div.innerHTML = ''
+    let count = 0
+    const item = document.getElementById('search-item').value
+    if(item != ''){
+        fetch('./color.json')
+        .then((res) => {return res.json()})
+        .then((data)=>{
+            for(let i = 0 ; i < data.color.length; i++){
+                if(data.color[i].type == String(item).toLowerCase()){
+                    div.innerHTML += 
+                    `<div class="color-container">
+                        <div class="color" style="background-color: ${data.color[i].hex}"></div>
+                        <div class="copy-wrapper" id="copy">
+                            <h3 id="${data.color[i].id}">${data.color[i].hex}</h3>
+                            <div id="${"s"+data.color[i].id}" style="display:none"><p>Copied!</p></div>
+                            <button onclick="copy('${data.color[i].id}','${data.color[i].hex}')"><i class="far fa-copy"></i></button>
+                        </div>
+                    </div>`
+                    count += 1
+                }
+            }
+            if(count == 0){
+                div.innerHTML = `<h3><i class="fas fa-dizzy" style="font-size:1.3em"></i> No results found</h3>`
+            }
+        })
+    }
+    else{
+        fetched()
+    }
 }
